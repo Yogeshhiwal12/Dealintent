d2e8079d16cc6c0a55afdf38eabdd3d7ed6d26f6
Yogeshhiwal12 <44598957+Yogeshhiwal12@users.noreply.github.com>
2023-05-10 14:25:44 +0530

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -96,7 +96,7 @@ const Layout: React.FC = () => {
             width: "40%",
             height: "40%",
            
-            left:"100px",
+            left:"80px",
           
             zIndex: -1,
 
